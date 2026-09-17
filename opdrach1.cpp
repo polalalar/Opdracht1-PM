79addd1999593782f37a03c78ffd130b222c9d9e
apenstad <thijmenrosenbrand@gmail.com>
2026-09-17 14:26:44 +0200

    Info blokje

--- opdrach1.cpp
+++ opdrach1.cpp
@@ -12,19 +12,24 @@ using namespace std;
 
 
 
-// Opdracht 1 - Programmeermethoden
-// Jens van der Linden & Thymen Roosenbrand
-// s5205212 & s5225752
-// Compiled with: g++ 13.3.0
+
+// Programma naam: Opdracht 1 - Programmeermethoden
+// Gemaakt door:Jens van der Linden & Thijmen Roosenbrand
+// Leerling nummer:s5205212 & s5225752
+// Compiled met: g++ 13.3.0
+// Versie 1.0
+// Laatste wijziging op: 17-9-2026
 
 
 
 int main()
 {
+	cout << "Dit programma is gemaakt door: Jens van der Linden & Thijmen Roosenbrand" << endl;
+	cout << "Dit programma is voor Opdracht 1 van programeer methoden" << endl;
+	cout << "Met dit programma kan je kijken of je geschikt bent voor een universitaire opleiding" << endl;
 	////////
 	// Infoblok ofzo
 	////
-
 
 
 	////////
